408d00be97954375178d3b77c1b1c0a25c895496
tairanhao <55215285+tairanhao@users.noreply.github.com>
2019-10-10 19:44:03 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	 for (var i = 20; i <= width; i = i + 30){
+		 x=20
+	while(x<width){
+		ellipse(x,i,15)
+		x=x+30;
+	}
+     }
+}
